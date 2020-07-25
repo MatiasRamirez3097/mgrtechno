5d9cc6f644835237a334e942274534f81259b424
MatiasRamirez <matiasgr3009@gmail.com>
2020-07-25 13:56:19 -0300

    datatable setting up

---
+++ resources/js/components/Slottop.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-toolbar flat>
+        <v-toolbar-title>{{title}}</v-toolbar-title>
+        <v-divider
+            class="mx-4"
+            inset
+            vertical
+        >
+        </v-divider>
+        <v-spacer></v-spacer>
+        <v-text-field
+            v-model="value"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+        >
+        </v-text-field>
+        <v-spacer></v-spacer>
+    </v-toolbar>
+</template>
+<script>
+    import {VToolbar,VToolbarTitle} from 'vuetify/lib'
+    export default {
+        data() {
+            return {
+                localValue: this.value
+            }
+        },
+        components: {
+            VToolbar,
+            VToolbarTitle
+        },
+        name: 'Slottop',
+        props:{
+            label: String,
+            search: String,
+            title: String,
+        },
+        watch: {
+            
+        }
+    }
+</script>
