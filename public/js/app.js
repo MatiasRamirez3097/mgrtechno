0477222c7dbbba8e6fb63523063456f97b56696f
Zero3009 <matiasgr97@Outlook.com>
2020-08-03 08:29:29 -0300

    push

--- public/js/app.js
+++ public/js/app.js
@@ -81487,7 +81487,7 @@ var routes = [{
   path: "/admin/productos"
 }, {
   component: function component() {
-    return __webpack_require__.e(/*! import() */ 8).then(__webpack_require__.bind(null, /*! ./pages/proveedores.vue */ "./resources/js/pages/proveedores.vue"));
+    return Promise.all(/*! import() */[__webpack_require__.e(0), __webpack_require__.e(1), __webpack_require__.e(7)]).then(__webpack_require__.bind(null, /*! ./pages/proveedores.vue */ "./resources/js/pages/proveedores.vue"));
   },
   path: "/proveedores"
 }];
